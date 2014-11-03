a1127d86e89391a59db671789675def4f45e05f4
Thang Pham <thang.pham@navigosgroup.com>
2014-11-03 15:15:09 +0700

    Add api to get status of request email

---
+++ users_status.php
@@ -0,0 +1,32 @@
+<?php
+
+$consumerId = 'your_consumer_id';
+$apiKey = 'your_api_key';
+$emailToCheck = 'test@email.com';
+
+$apiUrl = 'https://api.vietnamworks.com/users/status/?email='.urlencode($emailToCheck);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $apiUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // ignore SSL verification
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("CONSUMER-ID: $consumerId"));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("CONTENT-MD5: $apiKey"));
+
+$response = curl_exec($ch);
+$info = curl_getinfo($ch);
+
+// error case return code 400
+if ($info['http_code'] == 400) {
+    $response = json_decode($response, true);
+    echo 'Server returned an error with message: '.$response['message'];
+} elseif ($info['http_code'] == 200)  {
+    $response = json_decode($response, true);
+    echo 'Status of email '.$emailToCheck.' is: '.$response['accountStatus'];
+} else {
+    //unknow error
+    echo "Server returned unknown error: \n".print_r($info, true)."\nResponse: $response";
+}
+
+curl_close($ch);
